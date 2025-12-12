b0ec53c13414142a2f351a571bced201cc6d3a9f
Gwyneth Trenchard <gwyneth.trenchard@gmail.com>
2025-12-12 14:18:37 -0500

    fixed navigation on register page

--- frontend/src/Components/RegisterPage.jsx
+++ frontend/src/Components/RegisterPage.jsx
@@ -3,52 +3,50 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+export default function RegisterPage() {
+  // states
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  const [postResponse, setPostResponse] = useState("");
+  const navigate = useNavigate();
 
-export default function RegisterPage(){
-    // states
-    const [formData, setFormData] = useState({ username: "", password: "" });
-    const [postResponse, setPostResponse] = useState("");
-    const navigate = useNavigate();
+  //handlers
+  const handleOnChange = (e) => {
+    setFormData((prevData) => {
+      return { ...prevData, [e.target.name]: e.target.value };
+    });
+  };
 
-    //handlers
-    const handleOnChange = (e) => {
-        setFormData((prevData) => {
-            return { ...prevData, [e.target.name]: e.target.value };
-        });
-    };
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post("http://localhost:3000/create-user", {
+        ...formData,
+      });
+      setPostResponse(response.data.message);
 
-    const handleRegister = async () => {
-        try{
-            const response = await axios.post("http://localhost:3000/create-user", { 
-                ...formData,
-            });
-            setPostResponse(response.data.message);
-        
-            if(response.status === 201){
-                navigate("/");
-            }
-        
-        }catch(error){
-            setPostResponse(error.response.data.message || "Cannot create user");
-        }
-    };
+      if (response.status === 200) {
+        navigate("/");
+      }
+    } catch (error) {
+      setPostResponse(error.response.data.message || "Cannot create user");
+    }
+  };
 
-    const handleOnSubmit = (e) => {
-        e.preventDefault();
-        handleRegister()
-        setFormData({ username: "", password: "" })
-    };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    handleRegister();
+    setFormData({ username: "", password: "" });
+  };
 
-    return (
+  return (
     <div>
-        <FormComponent
-            formData={formData}
-            postResponse={postResponse}
-            handleOnChange={handleOnChange}
-            handleOnSubmit={handleOnSubmit}
-            currentPage="create-user"
-            nextPage=""
-        />
+      <FormComponent
+        formData={formData}
+        postResponse={postResponse}
+        handleOnChange={handleOnChange}
+        handleOnSubmit={handleOnSubmit}
+        currentPage="create-user"
+        nextPage=""
+      />
     </div>
-);
+  );
 }
