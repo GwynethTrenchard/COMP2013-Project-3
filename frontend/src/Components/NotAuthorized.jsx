6ac47273d6053b20c1a6b479992aef71ba928f86
Gwyneth Trenchard <gwyneth.trenchard@gmail.com>
2025-12-12 19:31:35 -0500

    fixed navigation page not found and not authorized

--- frontend/src/Components/NotAuthorized.jsx
+++ frontend/src/Components/NotAuthorized.jsx
@@ -2,7 +2,7 @@ export default function NotAuthorized() {
   return (
     <div>
       <h1>You are not authorized to view this page 😕</h1>
-      <a href="/main-page">Back to login page </a>
+      <a href="/">Back to login page </a>
     </div>
   );
 }
--- frontend/src/Components/PageNotFound.jsx
+++ frontend/src/Components/PageNotFound.jsx
@@ -2,7 +2,7 @@ export default function PageNotFound() {
   return (
     <div>
       <h1>Page Not Found</h1>
-      <a href="/main-page">Back to login page </a>
+      <a href="/">Back to login page </a>
     </div>
   );
 }
